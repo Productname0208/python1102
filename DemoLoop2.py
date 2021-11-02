0e485b8bdca10958c5db7edf00ddaad6e1503447
Productname0208 <juhyeok0208@gmail.com>
2021-11-02 15:42:12 +0900

    함수

--- DemoLoop2.py
+++ DemoLoop2.py
@@ -4,3 +4,12 @@
 lst = [1,2,3,4,5,6,7,8,9,10]
 result = [i**2 for i in lst if i>5]
 print(result)
+
+d = {100:"apple", 200:'kiwi'}
+print([v.upper() for v in d.values()])
+
+def times(a,b):
+    return a*b
+
+print(times(10,10))
+print(globals())
