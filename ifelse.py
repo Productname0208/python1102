00087d09966b7b5c0a4270cef6ef6ee86671fb24
Productname0208 <juhyeok0208@gmail.com>
2021-11-02 13:45:53 +0900

    반복1

--- ifelse.py
+++ ifelse.py
@@ -3,7 +3,7 @@
 print("---ctrl-c를 클릭하면 종료---")
 
 while True: # While 이용해서 keyboard interrupt 전까지 계속 뺑뺑이 돌릴수있음
-    score = int(input("점수를 입력: "))
+    score = float(input("점수를 입력: "))
 
     if 90 <= score <= 100:
         grade = "A"
