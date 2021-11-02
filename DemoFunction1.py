c3b87d68a6b288355b9fa37648ed12551d877b12
Productname0208 <juhyeok0208@gmail.com>
2021-11-02 15:57:21 +0900

    교집합 연습

--- DemoFunction1.py
+++ DemoFunction1.py
@@ -14,3 +14,15 @@ def swap(x,y):
 
 # 함수를 호출
 print(swap(5,6))
+
+# 디버깅 연습을 위한 함수
+def intersect(prelist, postlist):
+    result = []
+    for x in prelist:
+        if x in postlist and x not in result:
+            result.append(x)
+    return result
+
+# 함수를 호출
+print(intersect("HAM","SPAM"))
+print(intersect("가","나"))
