0aeb1f7fda6eeabd911b20d13f05646d07186594
Productname0208 <juhyeok0208@gmail.com>
2021-11-02 13:14:05 +0900

    211102 오후

--- ifelse.py
+++ ifelse.py
@@ -1,13 +1,17 @@
 # ifelse.py
 
-score = int(input("점수를 입력: "))
+print("---ctrl-c를 클릭하면 종료---")
 
-if 90 <= score <= 100:
-    grade = "A"
-elif 80 <= score:
-    grade = "B"
-     grace = "ab"
-else:
-    grade = "C"
+while True: # While 이용해서 keyboard interrupt 전까지 계속 뺑뺑이 돌릴수있음
+    score = int(input("점수를 입력: "))
 
-print("Grade is " + grade)
+    if 90 <= score <= 100:
+        grade = "A"
+    elif 80 <= score:
+        grade = "B"
+        grace = "ab"
+    else:
+        grade = "C"
+
+
+    print("Grade is " + grade)
