c656298f9b0c1e3f0dd1c4f99915d7248acff317
Productname0208 <juhyeok0208@gmail.com>
2021-11-02 14:39:01 +0900

    1102 오후2시

--- Demoloop.py
+++ Demoloop.py
@@ -30,3 +30,32 @@ for i in [2,3,4,5,6]:
     print("---%d단---" % i)
     for j in [1,2,3,4,5,6,7,8,9]:
         print("{0} * {1} = {2}".format(i,j,i*j))
+
+# 삼각형 출력
+for i in range(1,11):
+    print("*"*i)
+
+
+lst = [1,2,3,4,5,6,7,8,9,10]
+
+for i in lst:
+    # 다중라인 주석처리 ctrl+/
+    if i > 5:
+        break
+    print("item:{0}".format(i))
+
+print("---continue---")
+for i in lst:
+    if i % 2 == 0:
+        continue
+    print("item:%d" % i)
+
+# 수열함수
+result = list(range(2000,2022))
+print(result)
+day = list(range(1,32))
+print(day)
+
+# for ~ in ==> for 루프
+for i in range(1,10):
+    print(i)
