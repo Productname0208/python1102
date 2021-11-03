bc910a9b9e38401cbc143dcad9a5dda325a9a9c5
Productname0208 <juhyeok0208@gmail.com>
2021-11-03 14:45:51 +0900

    클래스에서 이름숨김 연습

--- BankAccount.py
+++ BankAccount.py
@@ -3,21 +3,23 @@
 #은행의 계정을 표현한 클래스 
 class BankAccount:
     def __init__(self, id, name, balance):
-        self.id = id
-        self.name = name 
-        self.balance = balance 
+        self.__id = id  # __붙이면 변수 숨김
+        self.__name = name 
+        self.__balance = balance 
     def deposit(self, amount):
-        self.balance += amount 
+        self.__balance += amount 
     def withdraw(self, amount):
-        self.balance -= amount
+        self.__balance -= amount
     def __str__(self):
-        return "{0}, {1}, {2}".format(self.id, 
-        self.name, self.balance)
+        return "{0}, {1}, {2}".format(self.__id, 
+        self.__name, self.__balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
+account1.deposit(5000)
 account1.withdraw(3000)
 print(account1)
-
-
-
+# 클래스 외부(인스턴스에서) 접근이 되는가? => 안 됨.
+# print(account1.__balance)
+# 클래스 외부에서는 _클래스명__변수명
+print(account1._BankAccount__balance)
