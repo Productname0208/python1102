f1af0521ff1699330961e1e390ebf4a84bff2349
Productname0208 <juhyeok0208@gmail.com>
2021-11-03 17:27:38 +0900

    클래스 연습 계속

--- 상속01.py
+++ 상속01.py
@@ -5,13 +5,22 @@ class Person:
     def printInfo(self):
         print("Info(Name:{0}, Phone Number: {1})".format(
             self.name, self.phoneNumber))
+    ab = 3
 
 class Student(Person):
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        # self.name = name
+        # self.phoneNumber = phoneNumber
+        Person.__init__(self,name,phoneNumber)
         self.subject = subject
         self.studentID = studentID
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(
+            self.name, self.phoneNumber))
+        print("Info(Name:{0}, Phone Number: {1})".format(
+            self.subject, self.studentID))
+        print(Person.ab)    # 변수도 상속받을 수 있음
+
 
 
 p = Person("전우치", "010-222-1234")
