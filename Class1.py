8bb2e7f1bcaa444ea40828163b97b3a9d6fb91bb
Productname0208 <juhyeok0208@gmail.com>
2021-11-03 11:23:35 +0900

    클래스 연습

--- Class1.py
+++ Class1.py
@@ -9,8 +9,11 @@ class Person:
     def print(self):
         print("이름은 ", self.name)
 
-# 2) 인스턴스 생성
+# 2) 인스턴스 생성. 클래스명() 하면 init을 부르도록 되어있음.
 p1 = Person()
 p2 = Person()
+
+# 3) 인스턴스 메서드 호출
+p1.name = "전우치"
 p1.print()
 p2.print()
